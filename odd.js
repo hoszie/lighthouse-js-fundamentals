f8aeb85691259a6a34a3defd483f1672e1725d0f
hoszie <nhoszko@gmail.com>
2019-09-02 22:46:06 +0000

    odd mail question

---
+++ odd.js
@@ -0,0 +1,12 @@
+function isOdd (num) {
+  return num % 2 != 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("1 is odd: " + isOdd(1));
+console.log("4 is odd: " + isOdd(4));
+console.log("5 is odd: " + isOdd(5));
+console.log("6 is odd: " + isOdd(6));
+console.log("7 is odd: " + isOdd(7));
+console.log("9 is odd: " + isOdd(9));
