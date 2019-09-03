d0ae2cc1ad0f4551798172edb58659e4c46d4bf8
hoszie <nhoszko@gmail.com>
2019-09-03 02:10:59 +0000

    ice cream

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+var newer = iceCreamFlavours.push("root beer");
+console.log(newer);
+
+// Your code below here...
--- odd.js
+++ odd.js
@@ -10,3 +10,53 @@ console.log("5 is odd: " + isOdd(5));
 console.log("6 is odd: " + isOdd(6));
 console.log("7 is odd: " + isOdd(7));
 console.log("9 is odd: " + isOdd(9));
+
+function ageCalculator (name, yearOfBirth, currentYear) {
+  var age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+}
+
+ageCalculator("Nick", 1987, 2019);
+
+
+function howManyHundreds (num) {
+  return Math.floor(num / 10);
+}
+
+howManyHundreds(424);
+howManyHundreds(1001);
+howManyHundreds(99);
+howManyHundreds(767);
+
+
+function calculateRectangleArea (length, width) {
+  if (length <= 0 || width <= 0) {
+    return "undefined";
+  } else {
+    return length * width;
+  }
+}
+
+
+function calculateTriangleArea (base, height) {
+  if (base <= 0 || height <= 0) {
+    return "undefined";
+  } else {
+    return (base * height) / 2;
+  }
+}
+
+
+function calculateCircleArea (radius) {
+  if (radius <= 0) {
+    return "undefined";
+  } else {
+    return Math.PI * (radius * radius);
+  }
+}
+
+
+calculateCircleArea(5);
+calculateRectangleArea(10, 4);
+calculateTriangleArea (8, 15);
+
